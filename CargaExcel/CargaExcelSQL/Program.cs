aad24b1a52d49588c171dd03ad332aa532d53b39
Chepe <jperalta@digitalgeko.com>
2025-10-03 15:22:45 -0600

    últimos cambios

--- CargaExcel/CargaExcelSQL/Program.cs
+++ CargaExcel/CargaExcelSQL/Program.cs
@@ -1,12 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Data;
+﻿using System.Data;
 using System.Data.SqlClient;
-using System.IO;
-using System.Linq;
 using System.Net;
 using System.Text.RegularExpressions;
-using System.Threading.Tasks;
 using ExcelDataReader;
 using MailKit.Net.Smtp;
 using MailKit.Security;
